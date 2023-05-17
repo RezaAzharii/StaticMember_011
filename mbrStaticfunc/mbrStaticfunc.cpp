e0dfa7894f9471a18a485d33baac02794cb9e698
RezaAzharii <rezaazhari867@gmail.com>
2023-05-17 17:32:14 +0700

    membuat method

--- mbrStaticfunc/mbrStaticfunc.cpp
+++ mbrStaticfunc/mbrStaticfunc.cpp
@@ -17,3 +17,14 @@ public:
 
 	mahasiswa(string pnama) :nama(pnama) { setID();}
 };
+
+int mahasiswa::nim = 0;
+void mahasiswa::printAll() {
+	id = ++nim;
+}
+
+void mahasiswa::printAll() {
+	cout << "ID		= " << id << endl;
+	cout << "Nama	= " << nama << endl;
+	cout << endl;
+}
