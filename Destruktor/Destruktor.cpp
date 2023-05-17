49ff7636ccb930a2c33fa916558bb1ccb8a47572
RezaAzharii <rezaazhari867@gmail.com>
2023-05-17 17:59:01 +0700

    memanggil destructor

--- Destruktor/Destruktor.cpp
+++ Destruktor/Destruktor.cpp
@@ -36,3 +36,11 @@ void angka::isiData() {
 		cout << i << " = "; cin >> arr[i];
 	}
 }
+
+int main() {
+	angka belajarcpp(3);
+	angka* ptrBelajarcpp = new angka(5);
+	delete ptrBelajarcpp;
+
+	return 0;
+}
